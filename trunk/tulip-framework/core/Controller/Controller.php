cc82304ebeb73892af96a08666bbe89b2c9534ca
barta.tamas.d@gmail.com <barta.tamas.d@gmail.com@3c8ceb18-1d3c-3b32-4f42-76cc87cf2ff2>
2011-08-09 14:31:23 +0000

    Controller loading mechanism ready.

--- trunk/tulip-framework/core/Controller/Controller.php
+++ trunk/tulip-framework/core/Controller/Controller.php
@@ -2,27 +2,51 @@
 
 namespace Controller;
 
-class Controller extends \Object
-{
+class Controller extends \Object {
 
 	public $composition = '';
 	public $view = '';
 
-	public static function loadController($path = REQUEST_URL)
+	public static function loadController( $path = REQUEST_URL )
 	{
 		$parameters = \explode('/', $path);
+		$parameters = \array_filter($parameters, '\strlen');
 		$parameterCount = \count($parameters);
-		for ($i = 1; $i <= $parameterCount; $i++) {
+		for ($i = $parameterCount; $i !== 0; $i--) {
 			try {
 				$controllerFullName = 'Controller\\';
+				$controllerFullName .= \implode('\\', \array_slice($parameters, 0, $i - 1));
+				$controllerFullName .= \ucwords($parameters[$i-1]) . 'Controller';
 				\System\AutoLoader::autoLoad($controllerFullName);
+				$controller = new $controllerFullName;
+				break;
 			} catch (\System\ErrorHandling\AutoLoaderException $e) {
 				
 			}
 		}
+		if ($i == 0) {
+			try {
+				$controllerFullName = 'Controller\\MainController';
+				\System\AutoLoader::autoLoad($controllerFullName);
+				$controller = new $controllerFullName;
+				$method = 'index';
+				\call_user_func_array(array( $controller, $method ), $parameters);
+			} catch (\System\ErrorHandling\AutoLoaderException $e) {
+				throw new \System\ErrorHandling\LoadControllerException($e);
+			}
+		} else {
+			if (isset($parameters[$i]) && \method_exists($controller, $parameters[$i])) {
+				$method = $parameters[$i];
+				$slicenum = $i + 1;
+			} else {
+				$method = 'index';
+				$slicenum = $i;
+			}
+			\call_user_func_array(array( $controller, $method ), \array_slice($parameters, $slicenum));
+		}
 	}
 
-	public function loadComposition($viewName = null, $compositionName = null)
+	public function loadComposition( $viewName = null, $compositionName = null )
 	{
 		$viewName = $viewName ? $viewName : $this->view;
 		$compositionName = $compositionName ? $compositionName : $this->composition;
@@ -30,7 +54,7 @@ class Controller extends \Object
 		$this->loadView($compositionName);
 	}
 
-	public function loadView($viewName = null)
+	public function loadView( $viewName = null )
 	{
 		$viewName = $viewName ? $viewName : $this->view;
 		$extensions = &\System\Tulip::$viewExtensions;
@@ -48,6 +72,16 @@ class Controller extends \Object
 		if (!$success) {
 			throw $exception;
 		}
+	}
+
+	protected function getControllerName()
+	{
+		return get_class($this);
+	}
+
+	public function index()
+	{
+		throw new \System\ErrorHandling\IndexNotOverridedException();
 	}
 
 }
