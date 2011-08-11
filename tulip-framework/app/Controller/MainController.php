79460131ca64b73582f1f3d25e9d061276d45503
barta.tamas.d@gmail.com <barta.tamas.d@gmail.com@3c8ceb18-1d3c-3b32-4f42-76cc87cf2ff2>
2011-08-11 13:51:42 +0000

    Controller loading mechanism ready.

--- tulip-framework/app/Controller/MainController.php
+++ tulip-framework/app/Controller/MainController.php
@@ -7,7 +7,21 @@ class MainController extends Controller
 
 	public function index()
 	{
-		$this->loadComposition();
+		$this->view['title'] = 'Tulip Demo 2012!';
+		$this->loadComposition('cump');
 	}
 
+	public function mindegmas()
+	{
+		$a = "cucc";
+		$c = "cuccC";
+		{
+			$b = "cuccB";
+			$c = "cuccCC";
+		}
+		echo $a;
+		echo $b;
+		echo $c;
+	}
+	
 }
